0b33ad0f94916b2d82c831d62758e95552fd8009
Lutiero <3720@ucdb.br>
2023-11-08 16:30:14 -0400

    customHook useLocalStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import React, {useState} from "react";
+import React from "react";
 import videoSrc from './video.mp4';
-import PlayerButton from "./Components/PlayerButton.tsx";
+import useLocalStorage from "./CustomHooks/useLocalStorage.tsx";
 
 function App() {
-
     const video = React.useRef<HTMLVideoElement>(null);
-    const [playing, setPlaying] = useState(false);
+    const [playing, setPlaying] = React.useState(false);
+    const [volume, setVolume] = useLocalStorage('volume', '0');
+
+    React.useEffect(() => {
+        if (!video.current) return;
+        if (Number(volume) > 1 || Number(volume) < 0) return;
+        video.current.volume = Number(volume);
+    }, [volume]);
+
     const controlPlayPause = () => {
         if (!playing) {
             video.current?.play()
@@ -15,51 +22,18 @@ function App() {
             setPlaying(false);
         }
     }
-    const forward = () => {
-        if (!video.current) return
-        video.current.currentTime += 2;
-        if (video.current.duration === video.current.currentTime) {
-            video.current.currentTime = 0
-        }
-    }
-    const playbackRate = (speed: number) => {
-        if (!video.current) return
-        video.current.playbackRate = speed;
-    }
-
-    function mute() {
-        if (!video.current) return;
-        video.current.muted = !video.current.muted;
-    }
-
-    const pictureAndPicture = async () => {
-        if (!video.current) return;
-        if (document.pictureInPictureElement) {
-            await document.exitPictureInPicture();
-        } else {
-            await video.current.requestPictureInPicture()
-        }
-    }
 
     return (
         <div>
             <div className='flex'>
-                <PlayerButton children={!playing ? 'Play' : 'Pause'}
-                              onClick={controlPlayPause}/>
-                <PlayerButton children='+ 2s'
-                              onClick={forward}/>
-                <PlayerButton children='1x'
-                              onClick={() => playbackRate(1)}/>
-                <PlayerButton children='2x'
-                              onClick={() => playbackRate(2)}/>
-                <PlayerButton children='M'
-                              onClick={mute}/>
-                <PlayerButton children='PiP'
-                              onClick={pictureAndPicture}/>
+                <button onClick={controlPlayPause}>{playing ? 'Pause' : 'Play'}</button>
+                <button onClick={() => setVolume("0")}>0</button>
+                <button onClick={() => setVolume("0.5")}>50%</button>
+                <button onClick={() => setVolume("1")}>100%</button>
+                <button onClick={() => setVolume("1.1")}>200%</button>
             </div>
             <video src={videoSrc}
                    ref={video}>
-
             </video>
         </div>
     );
